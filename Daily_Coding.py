b3794b85f63729098ec84f67578e221ee700d371
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2024-01-04 14:55:08 +1100

    机器人行走

--- Daily_Coding.py
+++ Daily_Coding.py
@@ -1,19 +1,11 @@
-# ops = ["5","-2","4","C","D","9","+","+"]
-# ops = ["1"]
-ops = ["5","2","C","D","+"]
+point = (0, 0)
 
-# C :delete the latest point
-# D: double the latest two scores
-# +: add latest two scores
-result = []
-for i in range(len(ops)):
-    if ops[i]=='C':
-        result.pop(-1)
-    elif ops[i]=='D':
-        result.append(2*result[-1])
-    elif ops[i]=='+':
-        result.append(result[-1]+result[-2])
-    else:
-        result.append(int(ops[i]))
-print(sum(result))
+steps = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
+from collections import Counter
 
+moves = "LR"
+def f2():
+    numbers = Counter(moves)
+    print(numbers)
+    return numbers['L'] == numbers['R'] and numbers['U'] == numbers['D']
+f2()
