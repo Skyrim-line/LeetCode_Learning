b257cd38add3548663bffe4714914ade19284b8b
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2024-03-07 19:38:46 +1100

    Latest push from the terminal

---
+++ COMP902123T3/sup4.py
@@ -0,0 +1,19 @@
+#将输入的格式为 4i7o的字符串识别转换成二级制数字再转成十进制,i代表1，o代表0
+
+def weird_num(x):
+    '''
+    >>>weird_num('4i7o')
+    This is a weird form of 11110000000 , the base 10 is 1920.
+    >>>weird_num('3i2o7o2i')
+    This is a weird form of 11100000000011 , the base 10 is 14339.
+    >>>weird_num('3i2o27i4o')
+    This is a weird form of 111001111111111111111111111111110000 , the base 10 is 62277025776.
+    '''
+    # insert your code here
+
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
